4bc1212b6bd7bb8bf9a953e1623647d15c6d72ec
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-06-02 12:56:40 +0200

    inicio 02/06

---
+++ src/main/webapp/script/script.js
@@ -0,0 +1,54 @@
+
+/*
+class Paciente{	
+	constructor(nombre,telefonoDueño,fechaAlta,fechaSalida){		
+		this.nombre=nombre;
+		this.telefonoDueño=telefonoDueño;
+		this.fechaAlta=fechaAlta;
+		this.fechaSalida=fechaSalida;		
+	}	
+}
+*/
+
+
+	
+		
+	
+function MostrarListado(){
+	
+	
+	var p1 = new Paciente("lucas","346326347","2-4-634","8-4-22");
+	var p2 = new Paciente("cat","65673463247","2-4-63","4-325-32");
+	
+	var lista;
+	lista.add(p1);
+	lista.add(p2);
+		
+	console.log("sadasd");
+	
+	document.write(p1.nombre);
+	
+	document.write(lista[1].nombre);
+	
+	document.write("assdasd");
+	
+}
+
+function a(){
+	document.write("assdasd");
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
