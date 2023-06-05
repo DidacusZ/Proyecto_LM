c5108677aaac88a004a5d158eaada8c580951b6d
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-06-05 08:58:36 +0200

    v5 falta css

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -27,26 +27,27 @@ function MostrarListado(){
 			document.write("<br> Fecha de alta: "+objP.fechaAlta);
 			document.write("<br> Fecha de salida: "+objP.fechaSalida+"<br><br>");			
 		}
-	}
-	
+	}	
 	//boton en la esquina derecha
 	document.write("<a style='position: absolute;  top: 10px; right: 10px;' href='formularioIngreso.html'><button>Nuevo ingreso</button></a>");	
 }
 
-function GuardarDatos(form){
+function GuardarDatos(form,id){
 	
 	let nombre = form.nombre.value;
 	let telefono = form.telefono.value;
-	let fechaAlta = form.fechaAlta.value;
+	let fechaAlta = form.fechaAlta.value;	
 	let fechaSalida = form.fechaSalida.value;
+	
+	if(fechaSalida=="")
+		fechaSalida="-"
 	
 	var objPaciente = new Paciente(nombre,telefono,fechaAlta,fechaSalida);
 	
-	var id = CalculoId()
 	//guardamos objeto
 	localStorage.setItem(id, JSON.stringify(objPaciente));
 
-	window.open("index.html");	
+	window.open("index.html");
 }
 
 
@@ -54,22 +55,36 @@ function BuscarPaciente(form){
 	
 		let telefono = form.telefono.value;
 		
-		id = localStorage.getItem("id");//accede al espacio en memoria para esa opcion (string)
-		nId = parseInt(id);//lo pasamos a int
+		let id = localStorage.getItem("id");//accede al espacio en memoria para esa opcion (string)
+		let nId = parseInt(id);//lo pasamos a int
+		let x;
+		console.log("1")
 	
 		for(let i=1;i<=nId;i++)
-		{		
+		{					
+			objP=JSON.parse(localStorage.getItem(i));
+			console.log("2")
 			
-			//objP=JSON.parse(localStorage.getItem(i));
-			
-			//if(objP.telefonoDuenio==telefono){				
-				
-			//}
+			if(objP.telefonoDuenio==telefono){
+				console.log("3")
+				document.write(" Paciente id = "+i);
+				document.write("<br> Nombre: "+objP.nombre);
+				document.write("<br> Telefono: "+objP.telefonoDuenio);
+				document.write("<br> Fecha de alta: "+objP.fechaAlta);
+				document.write("<br> Fecha de salida: "+objP.fechaSalida+"<br><br>");				
+				x=i;				
+				break;
+			}
 		}
 		
+		document.write("<h1>Cambiar datos del paciente</h1>")
 		
-		Prueba();
-		
+	document.write('<form><label>Nombre de la mascota:</label><input type="text" size="50" name="nombre" id="id"  /> <br><br>'
+			+'<label>Telefono del duenio:</label> <input	type="text" size="50" name="telefono"  />	<br><br>'
+			+'<label>fecha de alta (dd-mm-yyyy):</label> <input	type="text" size="50" name="fechaAlta"  />'
+			+'<br><br> <label>fecha de salida (dd-mm-yyyy):</label>	<input	type="text" size="50" name="fechaSalida"  />'
+			+'<br><br>	<input type="submit" value="Guardar" name="datos" onClick="GuardarDatos(this.form,'+x+')" /> </form>');
+	
 }
 
 function CalculoId(){
@@ -84,7 +99,6 @@ function CalculoId(){
 	numero = numero + 1;//lo incrementamos en 1
 	localStorage.setItem("id", numero);// lo guardamos en su espacio en memoria correspondiente	
 
-console.log(id)
 	return id;		
 }
 
@@ -96,8 +110,7 @@ var nuevoParrafo = document.createElement("p");
 // Crea un nodo de texto con el contenido que deseas imprimir
 
 var contenido = document.createTextNode(
-	"Hola, mundo "+" cccccccccccccccccccHola, mundo"+"Hola, mundo"	
-	);
+	"Hola, mundo "+" cHola, mundo"+"Hola, mundo");
 
 // Añade el nodo de texto al elemento <p>
 nuevoParrafo.appendChild(contenido);
